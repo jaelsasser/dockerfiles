0ad39d3a3ffaaf8a23e027dc462e4c0a28afc15f
Josh Elsasser <josh@elsasser.ca>
2016-07-01 19:35:31 -0700

    bitlbee fixup

--- bitlbee/docker-entrypoint.sh
+++ bitlbee/docker-entrypoint.sh
@@ -14,11 +14,16 @@ if [ ! -f "${DATADIR}/motd.txt" ]; then
     cp /etc/bitlbee/motd.txt.default "${DATADIR}/motd.txt"
 fi
 
-
 # Make sure $DATADIR is owned by bitlbee user. This effects ownership of the
 # mounted directory on the host machine too.
 echo "Setting necessary permissions..."
-chown -R znc:znc "$DATADIR"
+chown -R bitlbee:bitlbee "$DATADIR"
+
+if [ ! -f /var/run/bitlbee.pid ]; then
+    mkdir -p /var/run/
+    touch /var/run/bitlbee.pid
+    chown bitlbee:bitlbee /var/run/bitlbee.pid
+fi
 
 echo "Starting Bitlbee..."
-exec sudo -u bitlbee bitlbee -c "${DATADIR}/bitlbee.conf" -d "${DATADIR}/users" $@
+exec sudo -u bitlbee bitlbee -Fnv -c "${DATADIR}/bitlbee.conf" -d "${DATADIR}/users" $@
